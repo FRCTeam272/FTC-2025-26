e6fd6ef47bd93180f79fe6641e40f5a7bba45639
rocketbunnyst <rocketbunnyst@gmail.com>
2025-09-28 14:48:44 -0400

    Auto and Test Files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BlueFarTest.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive2024;
+
+
+@Config
+@Autonomous (name="BlueFarTest", group="Auto")
+public class BlueFarTest extends LinearOpMode {
+
+    //TODO - Coordinate List (Pasted from MeepMeep!)
+
+    // Starting Coordinates
+    double startX = 62;
+    double startY = -15;
+    double startH = Math.toRadians(180);
+
+    // Shoot Preload
+    double preloadX = 58;
+    double preloadY = -15;
+    double preloadH = Math.toRadians(210);
+
+    // Pickup Load1
+    double load1X = 35;
+    double load1Y = -30;
+    double load1H = Math.toRadians(270);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Initializing Robot
+        Pose2d StartPose = new Pose2d(startX,startY,startH);
+        MecanumDrive2024 drive = new MecanumDrive2024(hardwareMap, StartPose);
+
+        // TODO Build Trajectories - paste from MeepMeep, separating out by movement,
+        // because robot will do other actions timed by where in the trajectory it is
+
+        Action goToShootPreload = drive.actionBuilder(StartPose)
+                .strafeToLinearHeading(new Vector2d(preloadX,preloadY),preloadH) //drive to preload shooting position
+                .build();
+
+        Action goToIntakeLoad1 = drive.actionBuilder(drive.localizer.getPose())
+                .strafeToLinearHeading(new Vector2d(load1X,load1Y),load1H) //drive to position to loading 1st set of artifacts
+                .build();
+
+        while (!isStopRequested() && !opModeIsActive()) {
+            telemetry.addData("Position during Init", StartPose);
+            telemetry.update();
+        }
+
+        telemetry.addData("Starting Position", StartPose);
+        telemetry.update();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        // TODO Build the actual list of actions with Sequential and Parallel Actions
+        // Use SleepAction (the number in () is seconds) as placeholders, but many will need to stay because it will move on to the next action
+        // as soon as the prior non-movement action is started
+
+
+        Actions.runBlocking(new SequentialAction( //overall sequential action that continues for length of Auton
+
+                // drive to shoot preload while spinning up motor wheel at the same time
+                new ParallelAction(
+                        new SleepAction(3), // placeholder for spinning up shooter flywheel Action
+                        goToShootPreload
+                ),
+
+                // shoot 3 Artifacts from far position
+                new SleepAction(2), //placeholder for ShootArtifactFar
+
+                // drive to intake Load 1
+                goToIntakeLoad1,
+
+                // spin up Intake
+                new SleepAction(1) //spin up Intake Placeholder
+
+                // Drive forward intaking Artifacts
+
+        ));
+
+    }
+}
