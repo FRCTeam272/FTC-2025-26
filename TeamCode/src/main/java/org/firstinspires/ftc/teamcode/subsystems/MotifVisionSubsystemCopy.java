b8af475a01ce7a53aa9d34bd61078b131bf92431
rocketbunnyst <rocketbunnyst@gmail.com>
2025-11-20 17:27:39 -0500

    Motif Auto v2 and auto example

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MotifVisionSubsystemCopy.java
@@ -0,0 +1,138 @@
+/* Copyright (c) 2023 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.util.AprilTagEnums;
+import org.firstinspires.ftc.teamcode.util.MatchSettings;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+public class MotifVisionSubsystemCopy {
+
+    public final MatchSettings matchSettings;
+    public WebcamName webCam;
+    public static String motifTagSequence = "NONE";
+    boolean targetFound;
+
+    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
+    private static final int DESIRED_TAG_ID = -1;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    private VisionPortal visionPortal;               // Used to manage the video source.
+    private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
+    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
+
+    public MotifVisionSubsystemCopy(HardwareMap hardwareMap, Telemetry telemetry, MatchSettings matchSettings) {
+        this.matchSettings = matchSettings;
+        webCam = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+
+        // Initialize the Apriltag Detection process
+        initAprilTag();
+
+
+        // Wait for driver to press start
+        telemetry.addData("Camera preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch START to start OpMode");
+        telemetry.update();
+    }
+
+    public void scanObeliskTagSequence() {
+
+        desiredTag = null;
+
+        MatchSettings.Motif detectedMotif = MatchSettings.Motif.UNKNOWN;
+
+        // Step through the list of detected tags and look for a matching tag
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        for (AprilTagDetection detectedTag : currentDetections) {
+            if (detectedTag.id == AprilTagEnums.OBELISK_TAG_21.getId()
+                    || detectedTag.id == AprilTagEnums.OBELISK_TAG_22.getId()
+                    || detectedTag.id == AprilTagEnums.OBELISK_TAG_23.getId()) {
+                // Yes, we want to use this tag.
+                targetFound = true;
+                desiredTag = detectedTag;
+                // don't look any further.
+
+                if (desiredTag.id == AprilTagEnums.OBELISK_TAG_21.getId()) {
+                    motifTagSequence = AprilTagEnums.OBELISK_TAG_21.getDescription();
+                    detectedMotif = MatchSettings.Motif.GPP;
+                } else if (desiredTag.id == AprilTagEnums.OBELISK_TAG_22.getId()) {
+                    motifTagSequence = AprilTagEnums.OBELISK_TAG_22.getDescription();
+                    detectedMotif = MatchSettings.Motif.PGP;
+                } else if (desiredTag.id == AprilTagEnums.OBELISK_TAG_23.getId()) {
+                    motifTagSequence = AprilTagEnums.OBELISK_TAG_23.getDescription();
+                    detectedMotif = MatchSettings.Motif.PPG;
+                }
+                break;
+            }
+        }
+        matchSettings.setMotif(detectedMotif);
+    }
+
+
+    /**
+     * Initialize the AprilTag processor.
+     */
+    private void initAprilTag() {
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        //webCam = hardwaremap.get(WebcamName.class, "Webcam 1");
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // e.g. Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTag.setDecimation(1);
+
+        // Create the vision portal by using a builder.
+        if (USE_WEBCAM) {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(webCam)
+                    .addProcessor(aprilTag)
+                    .build();
+        } else {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(BuiltinCameraDirection.BACK)
+                    .addProcessor(aprilTag)
+                    .build();
+        }
+    }
+}
